26d90b030d7c48fdec8c744bd1e706644ec3d8e1
niyonx <kenkenysy@gmail.com>
2020-08-06 01:37:17 -0400

    Added dbfunctions.py

--- app.py
+++ app.py
@@ -1,49 +1,7 @@
 #!/usr/bin/env python
 from flask import Flask, url_for, render_template
-import sshtunnel
-import pymysql
-import config
+import dbfunctions
 import jinja2.exceptions
-
-sshtunnel.SSH_TIMEOUT = 30000
-
-server = sshtunnel.SSHTunnelForwarder(
-    'login.encs.concordia.ca',
-    ssh_username=config.SSH_USERNAME,
-    ssh_password=config.SSH_PASSWORD,
-    remote_bind_address=('qxc353.encs.concordia.ca', 3306)
-)
-
-server.start()
-print(server.local_bind_port)
-
-print("Connected to server")
-
-db = pymysql.connect(
-    host="localhost", 
-    port=server.local_bind_port,
-    user=config.DB_USER, 
-    password=config.DB_PASSWORD,
-    database=config.DB_DATABASE,
-    connect_timeout=3100)
-
-cursor = db.cursor()
-
-print("Connected to database")
-
-sql = "SELECT * FROM admin;"
-
-try:
-    # Execute the SQL command
-    cursor.execute(sql)
-    # Fetch all the rows in a list of lists.
-    results = cursor.fetchall()
-    # for item in results:
-    #     print(item)
-except:
-    print("Error: unable to fetch data")
-
-# db.close()
 
 app = Flask(__name__)
 @app.route('/')
@@ -56,21 +14,7 @@ def admin(pagename):
 
 @app.route('/viewJobs')
 def viewJobs():
-    sql = "SELECT * FROM jobs;"
-
-    results = []
-    try:
-        # Execute the SQL command
-        cursor.execute(sql)
-        # Fetch all the rows in a list of lists.
-        results = cursor.fetchall()
-        # for item in results:
-        #     print(item)
-    except:
-        print("Error: unable to fetch data")
-
-    # Table headers
-    field_names = [i[0] for i in cursor.description]
+    field_names, results = dbfunctions.getJobs()
 
     return render_template('viewJobs.html', jobs = results, jobsHeaders = field_names)
 
---
+++ dbfunctions.py
@@ -0,0 +1,64 @@
+import sshtunnel
+import pymysql
+import config
+
+sshtunnel.SSH_TIMEOUT = 30000
+
+# Connecting to database 
+
+server = sshtunnel.SSHTunnelForwarder(
+    'login.encs.concordia.ca',
+    ssh_username=config.SSH_USERNAME,
+    ssh_password=config.SSH_PASSWORD,
+    remote_bind_address=('qxc353.encs.concordia.ca', 3306)
+)
+
+server.start()
+print(server.local_bind_port)
+
+print("Connected to server")
+
+db = pymysql.connect(
+    host="localhost", 
+    port=server.local_bind_port,
+    user=config.DB_USER, 
+    password=config.DB_PASSWORD,
+    database=config.DB_DATABASE,
+    connect_timeout=3100)
+
+cursor = db.cursor()
+
+print("Connected to database")
+
+# sql = "SELECT * FROM admin;"
+
+# try:
+#     # Execute the SQL command
+#     cursor.execute(sql)
+#     # Fetch all the rows in a list of lists.
+#     results = cursor.fetchall()
+#     # for item in results:
+#     #     print(item)
+# except:
+#     print("Error: unable to fetch data")
+
+# db.close()
+
+def getJobs():
+    sql = "SELECT * FROM jobs;"
+
+    results = []
+    try:
+        # Execute the SQL command
+        cursor.execute(sql)
+        # Fetch all the rows in a list of lists.
+        results = cursor.fetchall()
+        # for item in results:
+        #     print(item)
+    except:
+        print("Error: unable to fetch data")
+
+    # Table headers
+    field_names = [i[0] for i in cursor.description]
+
+    return field_names, results
