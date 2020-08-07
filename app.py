3b7e8a9e7d694666b696aa784aca70f3a71b5f52
Nigel Yong <23243585+niyonx@users.noreply.github.com>
2020-08-07 15:32:01 -0400

    Revert "viewJobs almost done"

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, url_for, render_template, request, redirect, flash, session
-from datetime import date
+from flask import Flask, url_for, render_template, request, session
 import dbfunctions
 import jinja2.exceptions
 import secrets
@@ -16,6 +15,7 @@ def index():
 @app.route('/viewJobs')
 def viewJobs():
     field_names, results = dbfunctions.getJobs()
+
     return render_template('viewJobs.html', jobs = results, jobsHeaders = field_names)
 
 @app.route('/postJobs', methods=['POST'])
@@ -25,18 +25,6 @@ def postJob():
         dbfunctions.postJob(job_title)
         return render_template('postJobs.html',message="Success, job has been created!")
     return render_template('postJobs.html')
-
-@app.route('/applyJob', methods=['POST'])
-def applyJob():
-    if(request.form is not None):
-        jobSeekerID = request.form['jobSeekerID']
-        jobID = request.form['jobID']
-        appliedDate = date.today().strftime("%Y-%m-%d")
-        coverLetter = 'coverletter path'
-        resume = 'resume path'
-    dbfunctions.applyJob(jobSeekerID, jobID, appliedDate, coverLetter, resume)
-    flash('Job applied successfully!')
-    return redirect(url_for('viewJobs'))
 
 @app.errorhandler(jinja2.exceptions.TemplateNotFound)
 def template_not_found(e):
